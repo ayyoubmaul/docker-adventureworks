fe6c32c2f7ad24c222b888261541dc05a0d49164
Ayyoub Maulana <74217351+ayyoubmaul@users.noreply.github.com>
2026-09-07 21:36:47 +0700

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+if [ ! -f adventureworks.zip ]; then
+    echo "Downloading postgres AdventureWorks zip file from github.com/chriseaton/docker-adventureworks";
+    wget "https://github.com/chriseaton/docker-adventureworks/releases/download/2025-06-19/chriseaton-docker-adventureworks-postgres.zip" -O adventureworks.zip -q;
+    echo "Download complete."
+else
+    echo "postgres AdventureWorks file already downloaded. Skipping.";
+fi
+echo "Unzipping postgres AdventureWorks zip file.";
+unzip -o adventureworks.zip;
+rm adventureworks.zip;
+echo "Building postgres docker image.";
+if [[ -f install-a.sql ]] && [[ -f install-a.sql ]] && [[ -f install-a.sql ]] && [[ -f install-a.sql ]]; then
+    podman build  --progress=plain . -t chriseaton/adventureworks:postgres -t chriseaton/adventureworks:postgres-16;
+else
+    echo "Required SQL files not found. Please ensure install-a.sql, install-b.sql, install-c.sql, and install-d.sql are present.";
+    exit 1;
+fi
+echo 0;
